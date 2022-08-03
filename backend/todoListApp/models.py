f718a63ed2655ef5ddb3c7526aa1e7a6f43664d0
karladuran28 <kaduran1998@gmail.com>
2022-08-03 10:55:33 -0500

    backend models, views, urls

--- backend/todoListApp/models.py
+++ backend/todoListApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+  descripcion = models.TextField()
+  isCompleted = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.descripcion
